7071b79a56f19f0947c4f21237702092a6336b4a
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-18 16:42:59 +0200

    redefine validation to task for, in order to set user of request as default creator, add view "task_done" to mark task as done, add validation to user class view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.http import HttpRequest, HttpResponseForbidden
+from django.http import HttpRequest, HttpResponseForbidden, HttpResponseBadRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -37,11 +37,11 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
 
 @login_required
 def my_tasks(request):
-    tasks = Task.objects.filter(assignees__id__in=(request.user.id, ))
+    tasks_as_assigner = Task.objects.filter(assignees__id__in=(request.user.id, ))
+    tasks = Task.objects.filter(creator_id=request.user.id).union(tasks_as_assigner)
     context = {
         "tasks": tasks,
         "tasks_count": tasks.count(),
-        "tasks_completed": tasks.filter(is_completed=True).count(),
     }
     return render(request, "my-tasks.html", context=context)
 
@@ -57,22 +57,35 @@ class UserUpdateForm(UserChangeForm):
         model = Worker
         fields = ["username", "email", "first_name", "last_name", "avatar"]
 
+
 class TaskForm(forms.ModelForm):
-    photos = forms.ClearableFileInput(attrs={"allow_multiple_selected": True})
     class Meta:
         model = Task
-        fields = "__all__"
-        widgets = {
+        fields = ("name", "description", "deadline", "priority", "task_type", "task_image", "assignees")
+        write_only_fields = ("creator", )
 
-            'photos': forms.ClearableFileInput(attrs={'allow_multiple_selected': True})
-
-        }
 
 class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     form_class = TaskForm
     template_name = "task-create.html"
-    success_url = reverse_lazy("task_manager:tasks")
+    success_url = reverse_lazy("task_manager:my-tasks")
+
+    def form_valid(self, form):
+        task = form.save(commit=False)
+        task.creator = self.request.user
+        task.save()
+        return HttpResponseRedirect(reverse_lazy("task_manager:my-tasks"))
+    # def post(self, request, *args, **kwargs):
+    #     form = TaskForm(request.POST)
+    #     if form.is_valid():
+    #         task = form.save(commit=False)
+    #         task.creator = request.user
+    #         task.save()
+    #         return reverse("task_manager:my-tasks")
+    #     return HttpResponseBadRequest()
+
+
 
 class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
@@ -80,6 +93,31 @@ class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "task-update.html"
     success_url = reverse_lazy("task_manager:tasks")
 
+    def get(self, request, *args, **kwargs):
+        if request.user != get_object_or_404(Task, pk=kwargs.get("pk")).creator:
+            return HttpResponseForbidden()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user != get_object_or_404(Task, pk=kwargs.get("pk")).creator:
+            return HttpResponseForbidden()
+        return super().post(request, *args, **kwargs)
+
+@login_required
+def task_done(request, *args, **kwargs):
+    task = get_object_or_404(Task, pk=kwargs.get("pk"))
+    if request.user != task.creator or request.user not in task.assignees.all():
+        return HttpResponseForbidden()
+    if request.method == "GET":
+        context = {
+            "task": task
+        }
+        return render(request, "task-confirm-done.html", context=context)
+    if request.method == "POST":
+        task.is_completed = True
+        task.save()
+        return HttpResponseRedirect(reverse_lazy("task_manager:my-tasks"))
+    return HttpResponseBadRequest()
 
 class UserCreateView(generic.CreateView):
     model = Worker
@@ -93,7 +131,17 @@ class UserUpdateView(LoginRequiredMixin, generic.UpdateView):
     form_class = UserUpdateForm
     template_name = "profile-update.html"
     success_url = reverse_lazy("task_manager:index")
-    #TODO ПЕРЕОПРЕДІЛИ ПОСТ
+
+    def get(self, request, *args, **kwargs):
+        if request.user != get_object_or_404(Worker, pk=kwargs.get("pk")):
+            return HttpResponseForbidden()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user != get_object_or_404(Worker, pk=kwargs.get("pk")):
+            return HttpResponseForbidden()
+        return super().post(request, *args, **kwargs)
+
 
 @login_required
 def profile(request):
@@ -116,9 +164,18 @@ class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
         pk = kwargs.get("pk")
         task = get_object_or_404(Task, pk=pk)
         if user not in task.assignees.all():
-            return HttpResponseForbidden
+            return HttpResponseForbidden()
         text = request.POST["content"]
         if text:
             Commentary.objects.create(user=user, task=task, content=text)
             return redirect("task_manager:task-detail", pk=pk)
+        return HttpResponseBadRequest()
 
+
+@login_required
+def team(request):
+    context = {
+        "team": Worker.objects.all(),
+        "quantity": Worker.objects.all().count()
+    }
+    return render(request, "team.html", context=context)
