18dbedeb687048ab9ca1453b6c5093ff853a9fe8
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-18 16:39:42 +0200

    customize task page, add more information about task and create task-confirm-done.html to mark task as Done

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from task_manager.views import index, tasks, my_tasks, TaskView, TaskDetailView, TaskCreateView, CommentaryCreateView, \
-    TaskUpdateView
+    TaskUpdateView, task_done, team
 
 urlpatterns = [
     path("", index, name="index"),
+    path("team/", team, name="team"),
     path("tasks/", TaskView.as_view(), name="tasks"),
     path("create-task/", TaskCreateView.as_view(), name="create-task"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/done/", task_done, name="task-done"),
     path("tasks/<int:pk>/create-comment/", CommentaryCreateView.as_view(), name="task-create-comment"),
     path("my-tasks/", my_tasks, name="my-tasks")
 ]
