d3336a2ab60900a4e9f2e9fd2bdd55a3b0945a16
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-24 16:39:30 +0200

    create forms.py and replace forms there

---
+++ task_manager/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from task_manager.models import Task, Worker
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = Worker
+        fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ["username", "email", "first_name", "last_name", "avatar", "linkedin_url", "github_url",
+                  "instagram_url", "telegram_url"]
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ("name", "description", "priority", "task_type", "task_image", "assignees")
