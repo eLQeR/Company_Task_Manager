164466a1be81bb4d6719a0e8952dd3d6aeb66efe
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-24 17:04:09 +0200

    replace function based views to class based

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from task_manager.views import index, tasks, my_tasks, TaskView, TaskDetailView, TaskCreateView, CommentaryCreateView, \
-    TaskUpdateView, task_done, team
+from task_manager.views import index, TasksView, TaskDetailView, TaskCreateView, CommentaryCreateView, \
+    TaskUpdateView, task_done, team, MyTasksView, TaskDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("team/", team, name="team"),
-    path("tasks/", TaskView.as_view(), name="tasks"),
+    path("tasks/", TasksView.as_view(), name="tasks"),
     path("create-task/", TaskCreateView.as_view(), name="create-task"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/<int:pk>/done/", task_done, name="task-done"),
     path("tasks/<int:pk>/create-comment/", CommentaryCreateView.as_view(), name="task-create-comment"),
-    path("my-tasks/", my_tasks, name="my-tasks")
+    path("my-tasks/", MyTasksView.as_view(), name="my-tasks")
 ]
 
 app_name = "task_manager"
