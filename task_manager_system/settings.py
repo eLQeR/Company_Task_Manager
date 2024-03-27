4ae7078d066c9b17471fdd5ccc42a6a6f177ef3d
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-27 21:38:14 +0200

    Set STATIC_ROOT and STATICFILES_STORAGE manually

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -130,9 +130,8 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static", "./static/css", "./static/img"]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
